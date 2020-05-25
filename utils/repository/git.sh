f05aa12277de47bff36bc08c57ed4f4f4cce49e1
Gilles Besançon <gilles.besancon@gmail.com>
2020-05-25 14:52:17 -0500

    Add script to rewrite authors in commit history

--- utils/repository/git.sh
+++ utils/repository/git.sh
@@ -130,17 +130,17 @@ git_change_author_info()
   NEW_EMAIL="$2"
   NEW_NAME="$3"
 
-  execute_command git filter-branch --env-filter '
-  if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+  git filter-branch -f --env-filter '
+  if [ "$GIT_COMMITTER_EMAIL" = "'"$OLD_EMAIL"'" ]
   then
-  export GIT_COMMITTER_NAME="$NEW_NAME"
-  export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+  export GIT_COMMITTER_NAME="'"$NEW_NAME"'"
+  export GIT_COMMITTER_EMAIL="'"$NEW_EMAIL"'"
   fi
-  if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+  if [ "$GIT_AUTHOR_EMAIL" = "'"$OLD_EMAIL"'" ]
   then
-  export GIT_AUTHOR_NAME="$NEW_NAME"
-  export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+  export GIT_AUTHOR_NAME="'"$NEW_NAME"'"
+  export GIT_AUTHOR_EMAIL="'"$NEW_EMAIL"'"
   fi
   ' --tag-name-filter cat -- --branches --tags
-  
+  git push --force --tags origin 'refs/heads/*'
 }
