314be4b4a1dd5f4a826b3aade86e8dfffc7382df
Gilles Besançon <gilles.besancon@gmail.com>
2020-11-28 20:53:34 -0600

    rewrite history, one repo at a time

--- rewrite_gistory.sh
+++ rewrite_gistory.sh
@@ -5,12 +5,6 @@ source $(dirname $(realpath ${BASH_SOURCE[0]}))/utils/repository/git.sh
 EMAIL="gilles.besancon@gmail.com"
 NAME="Gilles Besançon"
 
-pushd scripts
-git_change_author_info_by_email "GBesancon@slb.com" "$EMAIL" "$NAME"
-git_change_author_info_by_email "gbesancon@slb.com" "$EMAIL" "$NAME"
-git_change_author_info_by_name "Gilles Besancon" "$EMAIL" "$NAME"
-popd
-
 git_change_author_info_by_email "GBesancon@slb.com" "$EMAIL" "$NAME"
 git_change_author_info_by_email "gbesancon@slb.com" "$EMAIL" "$NAME"
 git_change_author_info_by_name "Gilles Besancon" "$EMAIL" "$NAME"
