3052f97d72a908ed1b056d797b94cee70f21cefc
Gilles Besancon <gilles.besancon@gmail.com>
2020-12-01 22:19:07 -0600

    Create markdown to pdf makefile

---
+++ makefile/single/tex/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#set -e
+#set -x
+
+source $(dirname $(realpath ${BASH_SOURCE[0]}))/../../../utils/apt.sh
+
+setup()
+{
+  result=0
+  echo Installing texlive
+  install_package texlive
+  install_package_result=$?
+  if [ $install_package_result -eq 0 ]
+  then
+    result=$install_package_result
+  else
+    result=$install_package_result
+  fi
+  return $result
+}
+
+setup
+exit_on_error $?
